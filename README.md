f407348a8335b6053d32e8975cd385adba8a0516
kyupid <iamscvv@gmail.com>
2022-09-06 19:45:39 +0900

    심심푸리 빠이널과 오쁜

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 심심해서 배워보는 심심푸리 땅콩틀린🥜
+
+### 1. final in JAVA VS open in Kotlin
+**final method in Java**: A method that cannot be overridden.
+
+**final class in Java**: A class that cannot be extended.
+
+Open classes and methods in Kotlin are equivalent to the opposite of final in Java, 
+an open method is overridable and an open class is extendable in Kotlin.
+
+요약: final 이 하는 역할의 딱 반대역할
+질문: 왜 이런 장치를 만들었을까?
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    print("hello")
+    print(" kotlin!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println()
+    val sum = sum1(1, 2)
+    println(sum)
+
+    // A function body can be an expression. Its return type is inferred.
+    // 간단한건 아래처럼 함수를 선언해도 될거같긴한데..음............
+    // 일케하면 장점이 뭐지?
+    fun sum2(a: Int, b: Int) = a + b
+    val sum2 = sum2(1, 100)
+    println(sum2)
+
+    // 컴파일러가 리턴타입 무시도 한다는건가?
+    printSum(1,2)
+
+    val a: Int = 1  // immediate assignment
+    val b = 2   // `Int` type is inferred
+    val c: Int  // Type required when no initializer is provided
+    c = 3       // deferred assignment
+
+    // val와 var의 차이?
+    // val -> readonly
+    // var -> reassign-able
+
+    val rectangle = Rectangle(5.0, 2.0)
+    println("The perimeter is ${rectangle.perimeter}")
+}
+
+fun sum1 (a: Int, b: Int): Int {
+    return a + b
+}
+
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
+}
+
+class Shape
+
+class Rectangle(var height: Double, var length: Double) {
+    var perimeter = (height + length) * 2
 }
